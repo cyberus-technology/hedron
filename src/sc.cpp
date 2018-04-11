tparfait2000/NOVA

Add support for cross CPU IPC

src/sc.cpp
@@ -47,6 +47,11 @@ Sc::Sc (Pd *own, mword sel, Ec *e, unsigned c, unsigned p, unsigned q) : Kobject
     trace (TRACE_SYSCALL, "SC:%p created (EC:%p CPU:%#x P:%#x Q:%#x)", this, e, c, p, q);
 }
 
+Sc::Sc (Pd *own, Ec *e, unsigned c, Sc *x) : Kobject (SC, static_cast<Space_obj *>(own), 0, 0x1, free), ec (e), cpu (c), prio (x->prio), budget (x->budget), left (x->left)
+{
+    trace (TRACE_SYSCALL, "SC:%p created (EC:%p CPU:%#x P:%#x Q:%#x) - xCPU", this, e, c, prio, budget / (Lapic::freq_bus / 1000));
+}
+
 void Sc::ready_enqueue (uint64 t)
 {
     assert (prio < priorities);

src/syscall.cpp
@@ -5,6 +5,7 @@
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
  * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2013 Alexander Boettcher, Genode Labs GmbH
  *
  * This file is part of the NOVA microhypervisor.
  *
@@ -35,6 +36,10 @@ template <Sys_regs::Status T>
 void Ec::sys_finish()
 {
     current->regs.set_status (T);
+
+    if (current->xcpu_sm)
+        xcpu_return();
+
     ret_user_sysexit();
 }
 
@@ -61,7 +66,7 @@ void Ec::delegate()
     Ec *src = C ? ec : current;
     Ec *dst = C ? current : ec;
 
-    bool user = C || dst->cont == ret_user_sysexit;
+    bool user = C || ((dst->cont == ret_user_sysexit) || (dst->cont == xcpu_return));
 
     dst->pd->xfer_items (src->pd,
                          user ? dst->utcb->xlt : Crd (0),
@@ -108,17 +113,19 @@ void Ec::sys_call()
     Sys_call *s = static_cast<Sys_call *>(current->sys_regs());
 
     Kobject *obj = Space_obj::lookup (s->pt()).obj();
-    if (EXPECT_FALSE (obj->type() != Kobject::PT))
+    if (EXPECT_FALSE (obj->type() != Kobject::PT)) {
+        trace (TRACE_ERROR, "%s: Bad PT CAP (%#lx)", __func__, s->pt());
         sys_finish<Sys_regs::BAD_CAP>();
+    }
 
     Pt *pt = static_cast<Pt *>(obj);
     Ec *ec = pt->ec;
 
     if (EXPECT_FALSE (current->cpu != ec->xcpu))
-        sys_finish<Sys_regs::BAD_CPU>();
+        Ec::sys_xcpu_call();
 
     if (EXPECT_TRUE (!ec->cont)) {
-        current->cont = ret_user_sysexit;
+        current->cont = current->xcpu_sm ? xcpu_return : ret_user_sysexit;
         current->set_partner (ec);
         ec->cont = recv_user;
         ec->regs.set_pt (pt->id);
@@ -181,7 +188,9 @@ void Ec::sys_reply()
 
         Utcb *src = current->utcb;
 
-        if (EXPECT_TRUE (ec->cont == ret_user_sysexit))
+        assert (current->cont != ret_xcpu_reply);
+
+        if (EXPECT_TRUE ((ec->cont == ret_user_sysexit) || ec->cont == xcpu_return))
             src->save (ec->utcb);
         else if (ec->cont == ret_user_iret)
             src->save_exc (&ec->regs);
@@ -531,6 +540,49 @@ void Ec::sys_assign_gsi()
     r->set_msi (Gsi::set (gsi, r->cpu(), rid));
 
     sys_finish<Sys_regs::SUCCESS>();
+}
+
+void Ec::sys_xcpu_call()
+{
+    Sys_call *s = static_cast<Sys_call *>(current->sys_regs());
+
+    Kobject *obj = Space_obj::lookup (s->pt()).obj();
+    if (EXPECT_FALSE (obj->type() != Kobject::PT)) {
+        trace (TRACE_ERROR, "%s: Bad PT CAP (%#lx)", __func__, s->pt());
+        sys_finish<Sys_regs::BAD_CAP>();
+    }
+
+    Pt *pt = static_cast<Pt *>(obj);
+    Ec *ec = pt->ec;
+
+    if (EXPECT_FALSE (current->cpu == ec->cpu)) {
+        trace (TRACE_ERROR, "%s: Bad CPU", __func__);
+        sys_finish<Sys_regs::BAD_CPU>();
+    }
+
+    enum { UNUSED = 0, CNT = 0 };
+
+    current->xcpu_sm = new Sm (Pd::current, UNUSED, CNT);
+    current->cont = ret_xcpu_reply;
+
+    Ec *xcpu_ec = new Ec (Pd::current, Pd::current, Ec::sys_call, ec->cpu, current);
+    Sc *xcpu_sc = new Sc (Pd::current, xcpu_ec, xcpu_ec->cpu, Sc::current);
+
+    xcpu_sc->remote_enqueue();
+    current->xcpu_sm->dn (false);
+
+    ret_xcpu_reply();
+}
+
+void Ec::ret_xcpu_reply()
+{
+    assert (current->xcpu_sm);
+
+    delete current->xcpu_sm;
+    current->xcpu_sm = nullptr;
+
+    current->cont = ret_user_sysexit;
+    ret_user_sysexit();
 }
 
 extern "C"

cyberus-technology/hedron

src/sc.cpp
@@ -49,6 +49,11 @@ Sc::Sc (Pd *own, mword sel, Ec *e, unsigned c, unsigned p, unsigned q) : Kobject
     trace (TRACE_SYSCALL, "SC:%p created (EC:%p CPU:%#x P:%#x Q:%#x)", this, e, c, p, q);
 }
 
+Sc::Sc (Pd *own, Ec *e, unsigned c, Sc *x) : Kobject (SC, static_cast<Space_obj *>(own), 0, 0x1, free), ec (e), cpu (c), prio (x->prio), budget (x->budget), left (x->left)
+{
+    trace (TRACE_SYSCALL, "SC:%p created (EC:%p CPU:%#x P:%#x Q:%#llx) - xCPU", this, e, c, prio, budget / (Lapic::freq_bus / 1000));
+}
+
 void Sc::ready_enqueue (uint64 t, bool inc_ref, bool use_left)
 {
     assert (prio < priorities);

src/syscall.cpp
@@ -40,6 +40,10 @@ void Ec::sys_finish()
         current->clr_timeout();
 
     current->regs.set_status (S);
+
+    if (current->xcpu_sm)
+        xcpu_return();
+
     ret_user_sysexit();
 }
 
@@ -66,7 +70,7 @@ void Ec::delegate()
     Ec *src = C ? ec : current;
     Ec *dst = C ? current : ec;
 
-    bool user = C || dst->cont == ret_user_sysexit;
+    bool user = C || ((dst->cont == ret_user_sysexit) || (dst->cont == xcpu_return));
 
     dst->pd->xfer_items (src->pd,
                          user ? dst->utcb->xlt : Crd (0),
@@ -118,10 +122,10 @@ void Ec::sys_call()
     Ec *ec = pt->ec;
 
     if (EXPECT_FALSE (current->cpu != ec->xcpu))
-        sys_finish<Sys_regs::BAD_CPU>();
+        Ec::sys_xcpu_call();
 
     if (EXPECT_TRUE (!ec->cont)) {
-        current->cont = ret_user_sysexit;
+        current->cont = current->xcpu_sm ? xcpu_return : ret_user_sysexit;
         current->set_partner (ec);
         ec->cont = recv_user;
         ec->regs.set_pt (pt->id);
@@ -207,6 +211,8 @@ void Ec::sys_reply()
 
                 if (ec->cont == ret_user_sysexit)
                     ec->cont = sys_call;
+                else if (ec->cont == xcpu_return)
+                    ec->regs.set_status (Sys_regs::BAD_HYP, false);
             }
         }
 
@@ -217,7 +223,9 @@ void Ec::sys_reply()
 
         bool fpu = false;
 
-        if (EXPECT_TRUE (ec->cont == ret_user_sysexit))
+        assert (current->cont != ret_xcpu_reply);
+
+        if (EXPECT_TRUE ((ec->cont == ret_user_sysexit) || ec->cont == xcpu_return))
             src->save (ec->utcb);
         else if (ec->cont == ret_user_iret)
             fpu = src->save_exc (&ec->regs);
@@ -633,6 +641,53 @@ void Ec::sys_assign_gsi()
     r->set_msi (Gsi::set (gsi, r->cpu(), rid));
 
     sys_finish<Sys_regs::SUCCESS>();
+}
+
+void Ec::sys_xcpu_call()
+{
+    Sys_call *s = static_cast<Sys_call *>(current->sys_regs());
+
+    Kobject *obj = Space_obj::lookup (s->pt()).obj();
+    if (EXPECT_FALSE (obj->type() != Kobject::PT)) {
+        trace (TRACE_ERROR, "%s: Bad PT CAP (%#lx)", __func__, s->pt());
+        sys_finish<Sys_regs::BAD_CAP>();
+    }
+
+    Pt *pt = static_cast<Pt *>(obj);
+    Ec *ec = pt->ec;
+
+    if (EXPECT_FALSE (current->cpu == ec->cpu)) {
+        trace (TRACE_ERROR, "%s: Bad CPU", __func__);
+        sys_finish<Sys_regs::BAD_CPU>();
+    }
+
+    enum { UNUSED = 0, CNT = 0 };
+
+    current->xcpu_sm = new Sm (Pd::current, UNUSED, CNT);
+
+    Ec *xcpu_ec = new Ec (Pd::current, Pd::current, Ec::sys_call, ec->cpu, current);
+    Sc *xcpu_sc = new Sc (Pd::current, xcpu_ec, xcpu_ec->cpu, Sc::current);
+
+    xcpu_sc->remote_enqueue();
+    current->xcpu_sm->dn (false, 0);
+
+    ret_xcpu_reply();
+}
+
+void Ec::ret_xcpu_reply()
+{
+    assert (current->xcpu_sm);
+
+    delete current->xcpu_sm;
+    current->xcpu_sm = nullptr;
+
+    if (current->regs.status() != Sys_regs::SUCCESS) {
+        current->cont = sys_call;
+        current->regs.set_status (Sys_regs::SUCCESS, false);
+    } else
+        current->cont = ret_user_sysexit;
+
+    current->make_current();
 }
 
 extern "C"