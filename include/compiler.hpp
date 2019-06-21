91a1f6f58dd9c4197ce81c1ba02f8c9f6f1382fa
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2019-06-21 15:56:42 +0200

    compiler: disable ALWAYS_INLINE

--- include/compiler.hpp
+++ include/compiler.hpp
@@ -62,7 +62,7 @@
     #endif
 
         #define ALIGNED(X)          __attribute__((aligned(X)))
-        #define ALWAYS_INLINE       __attribute__((always_inline))
+        #define ALWAYS_INLINE
         #define CPULOCAL            __attribute__((section (".cpulocal,\"w\",@nobits#")))
         #define FORMAT(X,Y)         __attribute__((format (printf, (X),(Y))))
         #define INIT                __attribute__((section (".init")))
