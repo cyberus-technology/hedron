b5143b1d3d4c508880b1fcadab8989f1c2e95903
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2019-07-08 13:32:04 +0200

    unit: add catch2 framework

---
+++ test/unit/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Catch Main Function
+ *
+ * Copyright (C) 2019 Julian Stecklina, Cyberus Technology GmbH.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+// Do not write tests into this file. It is just meant to compile the
+// heavy-weight part of Catch.
