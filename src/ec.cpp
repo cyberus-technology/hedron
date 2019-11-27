ec1e20a300e9c5d0d5deefe11fa930d08800558b
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2019-11-27 13:24:31 +0200

    ec: mark UTCB and vLAPIC page as non-delegatable
    These pages need to be reclaimable by the kernel and we can only do
    that if we are able to remove all userspace mappings. The simple way
    forward is to allow only a single userspace mapping per page.

--- src/ec.cpp
+++ src/ec.cpp
@@ -64,7 +64,8 @@ Ec::Ec (Pd *own, mword sel, Pd *p, void (*f)(), unsigned c, unsigned e, mword u,
 
         utcb = new Utcb;
 
-        pd->Space_mem::insert (u, 0, Hpt::PTE_NX | Hpt::PTE_U | Hpt::PTE_W | Hpt::PTE_P, Buddy::ptr_to_phys (utcb));
+        pd->Space_mem::insert (u, 0, Hpt::PTE_NODELEG | Hpt::PTE_NX | Hpt::PTE_U | Hpt::PTE_W | Hpt::PTE_P,
+                               Buddy::ptr_to_phys (utcb));
 
         regs.dst_portal = NUM_EXC - 2;
 
@@ -107,7 +108,8 @@ Ec::Ec (Pd *own, mword sel, Pd *p, void (*f)(), unsigned c, unsigned e, mword u,
                 mword vlapic_page_p = Buddy::ptr_to_phys(vlapic_page);
 
                 Vmcs::write(Vmcs::APIC_VIRT_ADDR, vlapic_page_p);
-                pd->Space_mem::insert (u, 0, Hpt::PTE_NX | Hpt::PTE_U | Hpt::PTE_W | Hpt::PTE_P, vlapic_page_p);
+                pd->Space_mem::insert (u, 0, Hpt::PTE_NODELEG | Hpt::PTE_NX | Hpt::PTE_U | Hpt::PTE_W | Hpt::PTE_P,
+                                       vlapic_page_p);
 
                 if (use_apic_access_page) {
                     Vmcs::write(Vmcs::APIC_ACCS_ADDR, Buddy::ptr_to_phys(pd->get_access_page()));
