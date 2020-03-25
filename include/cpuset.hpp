cyberus-technology/hedron

commit 77a722e76c072f244689d7a6a51123333259266d

cpuset: static assert maximum size

include/cpuset.hpp
@@ -27,6 +27,7 @@ class Cpuset
 {
     private:
         mword val;
+        static_assert(sizeof(val) * 8 <= NUM_CPU, "NUM_CPU is too large");
 
     public:
         ALWAYS_INLINE

commit a1b25770b01aeaedb41ad699d0a76f296512e0d0

cpuset: fix static assertion
This assertion currently works, because we hit equality. Of course, we
need to have at least as many bits in the bitset as CPUs, so the
condition was reversed.

Fixes: 77a722e76c072f244689d7a6a51123333259266d ("cpuset: static assert maximum size")

include/cpuset.hpp
@@ -27,7 +27,7 @@ class Cpuset
 {
     private:
         mword val;
-        static_assert(sizeof(val) * 8 <= NUM_CPU, "NUM_CPU is too large");
+        static_assert(NUM_CPU <= sizeof(val) * 8, "NUM_CPU is too large");
 
     public:
         inline explicit Cpuset() : val (0) {}