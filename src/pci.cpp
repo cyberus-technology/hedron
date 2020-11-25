cyberus-technology/hedron

commit 00bb75c41cfe45b7e4d4e6325fda92fc1b629070

pci: use list iterators and algorithms
While I was here, I also moved most of the implementation into the
.cpp file.

src/pci.cpp
@@ -18,6 +18,7 @@
  * GNU General Public License version 2 for more details.
  */
 
+#include "algorithm.hpp"
 #include "pci.hpp"
 #include "pd.hpp"
 #include "stdio.hpp"
@@ -34,6 +35,15 @@ struct Pci::quirk_map Pci::map[] =
 {
 };
 
+Pci *Pci::find_dev (unsigned long r)
+{
+    auto range = Forward_list_range (list);
+    auto it = find_if (range.begin(), range.end(),
+                       [r] (auto &pci) { return pci.rid == r; });
+
+    return it != range.end() ? &*it : nullptr;
+}
+
 Pci::Pci (unsigned r, unsigned l) : Forward_list<Pci> (list), reg_base (hwdev_addr -= PAGE_SIZE), rid (static_cast<uint16>(r)), lev (static_cast<uint16>(l))
 {
     Pd::kern->claim_mmio_page (reg_base, cfg_base + (rid << PAGE_BITS), false);
@@ -41,6 +51,32 @@ Pci::Pci (unsigned r, unsigned l) : Forward_list<Pci> (list), reg_base (hwdev_ad
     for (unsigned i = 0; i < sizeof map / sizeof *map; i++)
         if (read<uint16>(REG_VID) == map[i].vid && read<uint16>(REG_DID) == map[i].did)
             (this->*map[i].func)();
+}
+
+void Pci::claim_all (Dmar *d)
+{
+    auto range = Forward_list_range (list);
+    for_each (range.begin(), range.end(),
+              [d] (Pci &pci) { if (not pci.dmar) { pci.dmar = d; } });
+}
+
+bool Pci::claim_dev (Dmar *d, unsigned r)
+{
+    Pci *pci = find_dev (r);
+
+    if (!pci)
+        return false;
+
+    unsigned l = pci->lev;
+
+    // Find the range of devices with a deeper level than the current device in
+    // the PCI hierarchy. Deeper means a numerically larger level.
+    Forward_list_range devices {pci};
+    auto end = find_if(devices.begin(), devices.end(), [l] (Pci &dev) { return dev.lev <= l; });
+
+    for_each(devices.begin(), end, [d] (Pci &dev) { dev.dmar = d; });
+
+    return true;
 }
 
 void Pci::init ()

include/pci.hpp
@@ -79,39 +79,16 @@ class Pci : public Forward_list<Pci>
         template <typename T>
         inline void write (Register r, T v) { *reinterpret_cast<T volatile *>(reg_base + r) = v; }
 
-        static inline Pci *find_dev (unsigned long r)
-        {
-            for (Pci *pci = list; pci; pci = pci->next)
-                if (pci->rid == r)
-                    return pci;
-
-            return nullptr;
-        }
+        static Pci *find_dev (unsigned long r);
 
     public:
         Pci (unsigned, unsigned);
 
         static inline void *operator new (size_t) { return cache.alloc(); }
 
-        static inline void claim_all (Dmar *d)
-        {
-            for (Pci *pci = list; pci; pci = pci->next)
-                if (!pci->dmar)
-                    pci->dmar = d;
-        }
+        static void claim_all (Dmar *d);
 
-        static inline bool claim_dev (Dmar *d, unsigned r)
-        {
-            Pci *pci = find_dev (r);
-
-            if (!pci)
-                return false;
-
-            unsigned l = pci->lev;
-            do pci->dmar = d; while ((pci = pci->next) && pci->lev > l);
-
-            return true;
-        }
+        static bool claim_dev (Dmar *d, unsigned r);
 
         static void init ();
 

commit c5c593127d8276ccd3474255006271193f35b25b

pci: fix regression in claim_dev
While refactoring this function to use algorithms, The original code
skipped the first device when looking for deeper PCI devices. The new
code does not. Because the current device is never deeper in the
hierarchy than itself, this results in no devices to be claimed.

Fix the code to skip the initial PCI device when looking for more
devices to claim.

Fixes: 00bb75c41cfe45b7e4d4e6325fda92fc1b629070 ("pci: use list iterators and algorithms")

src/pci.cpp
@@ -65,11 +65,13 @@ bool Pci::claim_dev (Dmar *d, unsigned r)
         return false;
 
     unsigned l = pci->lev;
-
-    // Find the range of devices with a deeper level than the current device in
-    // the PCI hierarchy. Deeper means a numerically larger level.
     Forward_list_range devices {pci};
-    auto end = find_if (devices, [l] (Pci const &dev) { return dev.lev <= l; });
+
+    // Find all following devices with a deeper level in the PCI hierarchy than
+    // the current device. Deeper means a numerically larger level.
+    assert (devices.begin() != devices.end());
+    auto end = find_if (++devices.begin(), devices.end(),
+                        [l] (Pci const &dev) { return dev.lev <= l; });
 
     for_each (devices.begin(), end, [d] (Pci &dev) { dev.dmar = d; });
 