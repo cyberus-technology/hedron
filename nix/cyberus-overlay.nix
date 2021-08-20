57bf851008a6f021d2079b6925d76d89c159e08d
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2021-08-20 14:18:52 +0300

    ci: limit the amount of exported derivations
    Most of the derivations we build for testing are not to be consumed.
    Limit the exported derivations to the default compiler and don't let
    the nixpkgs we overlay introduce any dependencies.

--- nix/cyberus-overlay.nix
+++ nix/cyberus-overlay.nix
@@ -1,1 +1,26 @@
-self: super: import ./release.nix { pkgs = self; }
+self: super:
+let
+  # We deliberately do not use the nixpkgs that we are overlaying to
+  # build Hedron, because this means that the user does not get the
+  # version we actually tested.
+  thisPackage = import ./release.nix {};
+
+  builds = thisPackage.hedron.builds;
+in {
+  hedron = super.pkgs.lib.warn
+    "Using the hedron attributes directly is deprecated. Use hedron.default instead."
+    {
+      inherit (builds) default-release default-debug;
+
+      builds = {
+        inherit (builds) default-release default-debug;
+      };
+    };
+
+  default = builds.default-release;
+  debug = builds.default-debug;
+
+  # As long as we do use our own nixpkgs to build all of the above,
+  # there is no reason to expose the integration tests, as they
+  # cannot show any different result than in our local pipeline.
+}
