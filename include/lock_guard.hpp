c94a100cf778a1e107441f6e38bf3ac43e549498
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2022-04-06 15:18:21 +0200

    treewide: replace NOVA with Hedron in license text

--- include/lock_guard.hpp
+++ include/lock_guard.hpp
@@ -6,13 +6,13 @@
  *
  * Copyright (C) 2015 Alexander Boettcher, Genode Labs GmbH
  *
- * This file is part of the NOVA microhypervisor.
+ * This file is part of the Hedron microhypervisor.
  *
- * NOVA is free software: you can redistribute it and/or modify it
+ * Hedron is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  *
- * NOVA is distributed in the hope that it will be useful,
+ * Hedron is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License version 2 for more details.
--- include/vmx_preemption_timer.hpp
+++ include/vmx_preemption_timer.hpp
@@ -5,11 +5,11 @@
  *
  * This file is part of the Hedron microhypervisor.
  *
- * NOVA is free software: you can redistribute it and/or modify it
+ * Hedron is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  *
- * NOVA is distributed in the hope that it will be useful,
+ * Hedron is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License version 2 for more details.
