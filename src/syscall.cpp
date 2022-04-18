22034dea945ebad1568e229edc2d7bb38ae8ec41
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2022-04-18 15:05:28 +0200

    gsi: enforce that userspace provides level/trigger mode on assign_gsi
    Current userspace always does this. Getting rid of the old behavior of
    falling back to defaults unlocks some simplifications in the interrupt
    subsystem.

--- src/syscall.cpp
+++ src/syscall.cpp
@@ -826,7 +826,11 @@ void Ec::sys_assign_gsi()
         sys_finish<Sys_regs::BAD_DEV>();
     }
 
-    if (r->has_configuration_override() and Gsi::gsi_table[gsi].ioapic) {
+    if (Gsi::gsi_table[gsi].ioapic) {
+        if (!r->has_configuration_override()) {
+            sys_finish<Sys_regs::BAD_PAR>();
+        }
+
         Gsi::set_polarity(gsi, r->level(), r->active_low());
     }
 
