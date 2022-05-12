7401f1c69a1247ff82c292eca4167f3d8f06ca3e
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2022-05-12 17:07:50 +0200

    atomic: allow to specify memory order

--- include/atomic.hpp
+++ include/atomic.hpp
@@ -23,51 +23,62 @@
 class Atomic
 {
 public:
-    template <typename T> static inline bool cmp_swap(T& ptr, T o, T n)
+    enum Memory_order
     {
-        return __atomic_compare_exchange_n(&ptr, &o, n, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+        SEQ_CST = __ATOMIC_SEQ_CST,
+        ACQUIRE = __ATOMIC_ACQUIRE,
+        RELEASE = __ATOMIC_RELEASE,
+        RELAXED = __ATOMIC_RELAXED,
+    };
+
+    template <typename T, Memory_order O = SEQ_CST> static inline bool cmp_swap(T& ptr, T o, T n)
+    {
+        return __atomic_compare_exchange_n(&ptr, &o, n, false, O, O);
     }
 
-    template <typename T> static inline T exchange(T& ptr, T n)
+    template <typename T, Memory_order O = SEQ_CST> static inline T exchange(T& ptr, T n)
     {
-        return __atomic_exchange_n(&ptr, n, __ATOMIC_SEQ_CST);
+        return __atomic_exchange_n(&ptr, n, O);
     }
 
-    template <typename T> static inline T load(T& ptr) { return __atomic_load_n(&ptr, __ATOMIC_SEQ_CST); }
-
-    template <typename T> static inline void store(T& ptr, T n)
+    template <typename T, Memory_order O = SEQ_CST> static inline T load(T& ptr)
     {
-        __atomic_store_n(&ptr, n, __ATOMIC_SEQ_CST);
+        return __atomic_load_n(&ptr, O);
     }
 
-    template <typename T> static inline T add(T& ptr, T v)
+    template <typename T, Memory_order O = SEQ_CST> static inline void store(T& ptr, T n)
     {
-        return __atomic_add_fetch(&ptr, v, __ATOMIC_SEQ_CST);
+        __atomic_store_n(&ptr, n, O);
     }
 
-    template <typename T> static inline T fetch_add(T& ptr, T v)
+    template <typename T, Memory_order O = SEQ_CST> static inline T add(T& ptr, T v)
     {
-        return __atomic_fetch_add(&ptr, v, __ATOMIC_SEQ_CST);
+        return __atomic_add_fetch(&ptr, v, O);
     }
 
-    template <typename T> static inline T sub(T& ptr, T v)
+    template <typename T, Memory_order O = SEQ_CST> static inline T fetch_add(T& ptr, T v)
     {
-        return __atomic_sub_fetch(&ptr, v, __ATOMIC_SEQ_CST);
+        return __atomic_fetch_add(&ptr, v, O);
     }
 
-    template <typename T> static inline void set_mask(T& ptr, T v)
+    template <typename T, Memory_order O = SEQ_CST> static inline T sub(T& ptr, T v)
     {
-        __atomic_fetch_or(&ptr, v, __ATOMIC_SEQ_CST);
+        return __atomic_sub_fetch(&ptr, v, O);
     }
 
-    template <typename T> static inline void clr_mask(T& ptr, T v)
+    template <typename T, Memory_order O = SEQ_CST> static inline void set_mask(T& ptr, T v)
     {
-        __atomic_fetch_and(&ptr, ~v, __ATOMIC_SEQ_CST);
+        __atomic_fetch_or(&ptr, v, O);
     }
 
-    template <typename T> static inline bool test_set_bit(T& val, unsigned long bit)
+    template <typename T, Memory_order O = SEQ_CST> static inline void clr_mask(T& ptr, T v)
+    {
+        __atomic_fetch_and(&ptr, ~v, O);
+    }
+
+    template <typename T, Memory_order O = SEQ_CST> static inline bool test_set_bit(T& val, unsigned long bit)
     {
         auto const bitmask{static_cast<T>(1) << bit};
-        return __atomic_fetch_or(&val, bitmask, __ATOMIC_SEQ_CST) & bitmask;
+        return __atomic_fetch_or(&val, bitmask, O) & bitmask;
     }
 };
