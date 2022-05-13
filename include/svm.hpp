cyberus-technology/hedron

commit 64748b0da0819deccf4f78cb4ce7bf0782e59c9d

cpulocal: move Vmcb variables

include/svm.hpp
@@ -65,10 +65,10 @@ class Vmcb
         char                reserved7[24];
         uint64              g_pat;
 
-        static Paddr        root        CPULOCAL;
-        static unsigned     asid_ctr    CPULOCAL;
-        static uint32       svm_version CPULOCAL;
-        static uint32       svm_feature CPULOCAL;
+        CPULOCAL_ACCESSOR(vmcb, root);
+        CPULOCAL_ACCESSOR(vmcb, asid_ctr);
+        CPULOCAL_ACCESSOR(vmcb, svm_version);
+        CPULOCAL_ACCESSOR(vmcb, svm_feature);
 
         static mword fix_cr0_set() { return 0; }
         static mword fix_cr0_clr() { return 0; }
@@ -144,7 +144,7 @@ class Vmcb
                 int_shadow = 0;
         }
 
-        static bool has_npt() { return Vmcb::svm_feature & 1; }
+        static bool has_npt() { return Vmcb::svm_feature() & 1; }
         static bool has_urg() { return true; }
 
         static void init();

src/svm.cpp
@@ -28,12 +28,7 @@
 #include "stdio.hpp"
 #include "svm.hpp"
 
-Paddr       Vmcb::root;
-unsigned    Vmcb::asid_ctr;
-uint32      Vmcb::svm_version;
-uint32      Vmcb::svm_feature;
-
-Vmcb::Vmcb (mword bmp, mword nptp) : base_io (bmp), asid (++asid_ctr), int_control (1ul << 24), npt_cr3 (nptp), efer (Cpu::EFER_SVME), g_pat (0x7040600070406ull)
+Vmcb::Vmcb (mword bmp, mword nptp) : base_io (bmp), asid (++asid_ctr()), int_control (1ul << 24), npt_cr3 (nptp), efer (Cpu::EFER_SVME), g_pat (0x7040600070406ull)
 {
     base_msr = Buddy::ptr_to_phys (Buddy::allocator.alloc (1, Buddy::FILL_1));
 }
@@ -51,7 +46,7 @@ void Vmcb::init()
     }
 
     Msr::write (Msr::IA32_EFER, Msr::read<uint32>(Msr::IA32_EFER) | Cpu::EFER_SVME);
-    Msr::write (Msr::AMD_SVM_HSAVE_PA, root = Buddy::ptr_to_phys (new Vmcb));
+    Msr::write (Msr::AMD_SVM_HSAVE_PA, root() = Buddy::ptr_to_phys (new Vmcb));
 
-    trace (TRACE_SVM, "VMCB:%#010lx REV:%#x NPT:%d", root, svm_version, has_npt());
+    trace (TRACE_SVM, "VMCB:%#010lx REV:%#x NPT:%d", root(), svm_version(), has_npt());
 }

include/cpulocal.hpp
@@ -91,6 +91,12 @@ struct alignas(PAGE_SIZE) Per_cpu {
     mword vmcs_fix_cr4_set;
     mword vmcs_fix_cr4_clr;
     mword vmcs_fix_cr4_mon;
+
+    // SVM-related variables
+    Paddr    vmcb_root;
+    unsigned vmcb_asid_ctr;
+    uint32   vmcb_svm_version;
+    uint32   vmcb_svm_feature;
 };
 
 static_assert(OFFSETOF(Per_cpu, self)            == PAGE_SIZE,

src/cpu.cpp
@@ -132,7 +132,7 @@ void Cpu::check_features()
     if (eax & 0x80000000) {
         switch (static_cast<uint8>(eax)) {
             default:
-                cpuid (0x8000000a, Vmcb::svm_version, ebx, ecx, Vmcb::svm_feature);
+                cpuid (0x8000000a, Vmcb::svm_version(), ebx, ecx, Vmcb::svm_feature());
                 FALL_THROUGH;
             case 0x4 ... 0x9:
                 cpuid (0x80000004, name[8], name[9], name[10], name[11]);

commit 0fc0e4681a8c26a36a9b18cf00cca1d966a7b944

svm: disable due to widespread brokenness
The last years of refactoring have left the SVM code in a sorry state.
At the moment, the SVM code is too buggy to leave enabled by default.

This commit fixes compilation for GCC 12, but also disables the SVM
code for now.

At least one regression was introduced with 64748b0da0819deccf4f78cb4ce7bf0782e59c9d.

include/svm.hpp
@@ -26,6 +26,12 @@
 class Vmcb
 {
 public:
+    // SVM support is currently in poor shape and disabled by default. If you want to experiment with it, you
+    // can disable this.
+    //
+    // If you want to find places to fix, grep for this variable in the code.
+    static constexpr bool DISABLE_BROKEN{true};
+
     union {
         char pad[1024];
         struct {

src/ec.cpp
@@ -407,6 +407,20 @@ void Ec::ret_user_vmrun()
         die("Invalid XCR0");
     }
 
+    // *** The SVM vCPU entry code is currently broken and needs repair. ***
+    //
+    // There are multiple issue with the code below:
+    //
+    // - LOAD_GPR loads RAX, but RAX needs to point to the VMCB.
+    // - GS is destroyed by VMRUN, so we cannot use it to restore the stack.
+    // - The Vmcb::root() memory reference generates a indirect memory operand via a general-purpose register,
+    //   but all of them are clobbered by VMRUN.
+    //
+    // All of these issues have been caused by refactoring and not being able to test SVM. So we keep it
+    // disabled for now until we have working tests on actual hardware.
+    if (Vmcb::DISABLE_BROKEN)
+        Console::panic("SVM vCPU entry is broken");
+
     // clang-format off
     asm volatile ("lea %0, %%rsp;"
                   EXPAND (LOAD_GPR)
@@ -422,7 +436,7 @@ void Ec::ret_user_vmrun()
                   "cli;"
                   "stgi;"
                   "jmp svm_handler;"
-                  : : "m" (current()->regs), "m" (Vmcb::root) : "memory");
+                  : : "m" (current()->regs), "m" (Vmcb::root()) : "memory");
     // clang-format on
 
     UNREACHED;

src/svm.cpp
@@ -41,7 +41,7 @@ Vmcb::~Vmcb()
 
 void Vmcb::init()
 {
-    if (not Cpu::feature(Cpu::FEAT_SVM) or not has_npt()) {
+    if (DISABLE_BROKEN or (not Cpu::feature(Cpu::FEAT_SVM) or not has_npt())) {
         Hip::clr_feature(Hip::FEAT_SVM);
         return;
     }