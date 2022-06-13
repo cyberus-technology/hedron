cyberus-technology/hedron

commit f7615a589f601353f0e2a7dfdf08d8753d2d4399

Bugfix: avoid assertion by checking sc priority
Kernel fails if Sc priority is higher/equal to the maximum (128 currently)

src/syscall.cpp
@@ -309,7 +309,7 @@ void Ec::sys_create_sc()
         sys_finish<Sys_regs::BAD_CAP>();
     }
 
-    if (EXPECT_FALSE (!r->qpd().prio() || !r->qpd().quantum())) {
+    if (EXPECT_FALSE (!r->qpd().prio() || !r->qpd().quantum() | (r->qpd().prio() >= Sc::priorities))) {
         trace (TRACE_ERROR, "%s: Invalid QPD", __func__);
         sys_finish<Sys_regs::BAD_PAR>();
     }

include/sc.hpp
@@ -36,12 +36,12 @@ class Sc : public Kobject, public Refcount
         uint64 const budget;
         uint64 time;
 
+        static unsigned const priorities = 128;
+
     private:
         uint64 left;
         Sc *prev, *next;
         uint64 tsc;
-
-        static unsigned const priorities = 128;
 
         static Slab_cache cache;
 

commit c672d92a2688938616868fda2a8d27cbe17d0a03

syscalls: fix create_sc parameter checking
The code is functionally correct, but the bitwise operator was clearly
used unintentionally.

Fixes: f7615a589f601353f0e2a7dfdf08d8753d2d4399

src/syscall.cpp
@@ -333,7 +333,7 @@ void Ec::sys_create_sc()
         sys_finish<Sys_regs::BAD_CAP>();
     }
 
-    if (EXPECT_FALSE(!r->qpd().prio() || !r->qpd().quantum() | (r->qpd().prio() >= NUM_PRIORITIES))) {
+    if (EXPECT_FALSE(!r->qpd().prio() || !r->qpd().quantum() || (r->qpd().prio() >= NUM_PRIORITIES))) {
         trace(TRACE_ERROR, "%s: Invalid QPD", __func__);
         sys_finish<Sys_regs::BAD_PAR>();
     }