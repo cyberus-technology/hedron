f450ebf3fd53a3ac39132ff2fdb5eb3cf53cc163
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2022-06-13 18:12:48 +0200

    spinlock: rewrite test using futures

--- test/unit/spinlock.cpp
+++ test/unit/spinlock.cpp
@@ -22,6 +22,7 @@
 #include <catch2/catch.hpp>
 #include <chrono>
 #include <cstdint>
+#include <future>
 #include <thread>
 #include <vector>
 
@@ -42,40 +43,45 @@ TEST_CASE("Spinlock smoke test", "[spinlock]")
 {
     static unsigned const thread_count{std::thread::hardware_concurrency()};
 
-    Spinlock l;
-
-    std::atomic<bool> should_exit{false};
-    std::vector<std::thread> threads;
-
     std::atomic<uint64_t> unsafe_counter{0};
     std::atomic<uint64_t> safe_counter{0};
 
-    for (size_t i = 0; i < thread_count; i++) {
-        threads.emplace_back([&l, &should_exit, &unsafe_counter, &safe_counter]() {
-            while (not should_exit.load(std::memory_order_relaxed)) {
-                l.lock();
+    {
+        Spinlock l;
+        std::atomic<bool> should_exit{false};
 
-                // Increment unsafe_counter in an obviously concurrency unsafe way. This is fine as long as
-                // the spinlock works. If not, we see it in the assertion below.
-                unsafe_counter.store(unsafe_counter.load(std::memory_order_relaxed) + 1,
-                                     std::memory_order_relaxed);
+        // The vector of futures must be declared last, so it is destroyed first. Otherwise, we can have
+        // destruction order bugs at the end of this scope, because the futures reference other local
+        // variables in this scope.
+        std::vector<std::future<void>> futures;
 
-                // To see whether the above counter lost some updates, keep another counter around that we
-                // increment the correct way.
-                safe_counter++;
+        std::generate_n(std::back_inserter(futures), thread_count, [&]() {
+            return std::async(std::launch::async, [&l, &should_exit, &unsafe_counter, &safe_counter]() {
+                while (not should_exit.load(std::memory_order_relaxed)) {
+                    l.lock();
 
-                l.unlock();
-            }
+                    // Increment unsafe_counter in an obviously concurrency unsafe way. This is fine as
+                    // long as the spinlock works. If not, we see it in the assertion below.
+                    unsafe_counter.store(unsafe_counter.load(std::memory_order_relaxed) + 1,
+                                         std::memory_order_relaxed);
+
+                    // To see whether the above counter lost some updates, keep another counter around
+                    // that we increment the correct way.
+                    safe_counter++;
+
+                    l.unlock();
+                }
+            });
         });
-    }
 
-    // We don't want to make the unit test run for long. In manual tests 100ms was more than enough to smoke
-    // out an deliberately broken spinlock. This may not find subtle issues, though.
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        // We don't want to make the unit test run for long. In manual tests 100ms was more than enough to
+        // smoke out a deliberately broken spinlock. This may not find subtle issues, though.
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-    should_exit = true;
-    for (auto& t : threads) {
-        t.join();
+        should_exit = true;
+
+        // The destructor of std::future will wait for the threads to finish. This is not always the
+        // case. Read the documentation for ~future(), before you depend on this elsewhere.
     }
 
     CHECK(unsafe_counter.load() == safe_counter.load());
