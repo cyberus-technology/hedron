fb8310d3999a9fcbfe0adb0bb2054415384225f8
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2022-08-09 16:37:45 +0200

    scope_guard: introduce scope exiting cleanup helper
    We have code locations where we want to apply certain cleanups
    regardless of how we exit the scope. For example, the delegate code
    wants to do TLB flushs regardless of whether sub-operations have
    failed.

---
+++ include/scope_guard.hpp
@@ -0,0 +1,76 @@
+/*
+ * A per-scope cleanup helper.
+ *
+ * Copyright (C) 2022 Julian Stecklina, Cyberus Technology GmbH.
+ *
+ * This file is part of the Hedron hypervisor.
+ *
+ * Hedron is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Hedron is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#include <util.hpp>
+
+// Execute cleanup code before exiting from a scope.
+//
+// The cleanup function that is given on construction will be executed by the destructor of Scope_guard
+// regardless how the scope is exited.
+//
+// Typical usage is:
+//
+// {
+//     Scope_guard g([] { ... do some cleanup ... })};
+//
+//     if (something) {
+//         // Cleanup happens here.
+//         return;
+//     }
+//
+//     // Cleanup happens here.
+// }
+//
+// BEWARE: Scope_guard vs [[noreturn]]
+//
+// It is usually an error to use a Scope_guard in a function that calls [[noreturn]] functions. In this case,
+// the destructor of Scope_guard is not called and will NOT call the cleanup function. A workaround is to
+// introduce a new scope in which to use Scope_guard that does not contain calls to [[noreturn]] functions.
+//
+// Especially in the system call layer, Hedron uses a lot of [[noreturn]] functions and Scope_guard should be
+// used with extra caution.
+//
+// BEWARE: Modifying state from Scope_guard cleanup functions.
+//
+// Consider the following example:
+//
+// int pitfall()
+// {
+//     int i {0};
+//     Scope_guard g{[&i] { i = 1; }};
+//
+//     return i;
+// }
+//
+// This function will return 0, not 1. This may be unexpected by developers. Conceptually, the return value is
+// computed and _then_ the destructors run.
+//
+// Code that relies on this behavior should be avoided to keep code easy to understand and read.
+template <typename F> class Scope_guard
+{
+    F scope_cleanup_fn;
+
+public:
+    Scope_guard() = delete;
+    Scope_guard(Scope_guard&) = delete;
+    Scope_guard& operator=(Scope_guard&) = delete;
+
+    Scope_guard(F&& f) : scope_cleanup_fn(move(f)) {}
+    Scope_guard(F const& f) : scope_cleanup_fn(f) {}
+
+    ~Scope_guard() { scope_cleanup_fn(); }
+};
