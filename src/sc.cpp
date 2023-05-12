4259b114cff1cfbacc0cd2122ee9eab0a417ce21
Sebastian Eydam <sebastian.eydam@cyberus-technology.de>
2023-05-12 14:58:00 +0200

    sc: don't reschedule when a SC with the same prio as Sc::current becomes ready to run

--- src/sc.cpp
+++ src/sc.cpp
@@ -73,8 +73,9 @@ void Sc::ready_enqueue(uint64 t, bool inc_ref)
     trace(TRACE_SCHEDULE, "ENQ:%p (%llu) PRIO:%#x TOP:%#x %s", this, left, prio, prio_top(),
           prio > current()->prio ? "reschedule" : "");
 
-    if (prio > current()->prio || (this != current() && prio == current()->prio && left))
+    if (prio > current()->prio) {
         Atomic::set_mask(Cpu::hazard(), HZD_SCHED);
+    }
 
     if (!left)
         left = budget;
