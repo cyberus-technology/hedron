0ea796e76e0abeb1aede8d10a44e17f7d1fc11ca
Sebastian Eydam <sebastian.eydam@cyberus-technology.de>
2023-06-05 13:45:57 +0200

    ioapic: remove unused code

--- include/ioapic.hpp
+++ include/ioapic.hpp
@@ -40,7 +40,6 @@ private:
     unsigned const gsi_base;
     unsigned const id;
     uint16 rid;
-    Spinlock lock;
 
     // An array of all IOAPICs in the system indexed by their ID.
     static No_destruct<Optional<Ioapic>> ioapics_by_id[NUM_IOAPIC];
@@ -86,15 +85,6 @@ private:
         IRT_POLARITY_ACTIVE_LOW = 1UL << 13,
     };
 
-    // A shadow copy of the IRT.
-    //
-    // Each write to the IRT must also be performed here. This saves us from expensive reads of the IRT when
-    // we want to mask an IRT entry.
-    //
-    // One bit that is not correctly shadowed is the 'Remote IRR' bit. We never set it ourselves, so we will
-    // always overwrite it with zero. This should be harmless.
-    Static_vector<uint64, IOAPIC_MAX_IRT> shadow_redir_table;
-
     inline void index(Register reg) { *reinterpret_cast<uint8 volatile*>(reg_base + IOAPIC_IDX) = reg; }
 
     inline uint32 read(Register reg)
@@ -122,35 +112,6 @@ private:
     // The returned value is one less then the count of pins.
     inline unsigned irt_max() { return read(IOAPIC_VER) >> 16 & 0xff; }
 
-    // Write an IRT entry to the IOAPIC without caching it.
-    //
-    // This has to be used with caution, because we rely on shadow_redir_table to reflect the actual state of
-    // the IRT.
-    void set_irt_entry_uncached(size_t entry, uint64 val);
-
-    // A special case of set_irt_entry_uncached that only sets the low 32-bit of the IRT entry.
-    //
-    // This uses 2 instead of 4 MMIO operations and should be significantly faster. This is useful, because
-    // the low 32-bit contain the frequently toggled mask bit.
-    void set_irt_entry_uncached_low(size_t entry, uint32 val);
-
-    // Write an IRT entry and update the write-through cache.
-    //
-    // This function may not re-write the full IRT entry, if the written value is not changed.
-    void set_irt_entry(size_t entry, uint64 val);
-
-    // Read an IRT entry from the write-through cache.
-    uint64 get_irt_entry(size_t entry) const;
-
-    // Set all IOAPIC pins to be masked.
-    void initialize_as_masked();
-
-    // Unconditionally write back the shadow copy of IRT entries to the IOAPIC.
-    //
-    // This is useful, when we are not sure what the IOAPIC state in the hardware is or we know there is a
-    // mismatch between our shadow copy and the hardware.
-    void sync_from_shadow();
-
 public:
     enum
     {
@@ -165,17 +126,6 @@ public:
     {
         assert(ioapic_id < NUM_IOAPIC);
         return *ioapics_by_id[ioapic_id];
-    }
-
-    static bool claim_dev(uint16 rid, uint8 ioapic_id)
-    {
-        if (not(ioapic_id < NUM_IOAPIC and ioapics_by_id[ioapic_id]->has_value()) or
-            (*ioapics_by_id[ioapic_id])->rid != 0) {
-            return false;
-        }
-
-        (*ioapics_by_id[ioapic_id])->rid = rid;
-        return true;
     }
 
     static inline void add_to_hip(Hip_ioapic*& entry)
@@ -195,25 +145,5 @@ public:
         }
     }
 
-    // Returns the number of usable pins on this IOAPIC.
-    uint8 pin_count() const { return static_cast<uint8>(shadow_redir_table.size()); }
-
     uint16 get_rid() const { return rid; }
-
-    // Configure an IRT entry.
-    //
-    // The IRT entry will be unmasked after this call.
-    //
-    // For context, see Section 5.1.2.1 "Interrupts in Compatibility Format" in the the VT-d specification.
-    void set_irt_entry_compatibility(uint8 ioapic_pin, unsigned apic_id, unsigned vector, bool level,
-                                     bool active_low);
-
-    // Mask or unmask a specific IOAPIC pin on one specific IOAPIC.
-    void set_mask(uint8 ioapic_pin, bool masked);
-
-    // Prepare all IOAPICs in the system for system suspend by saving their state to memory.
-    static void save_all();
-
-    // Restore the state of all IOAPICs from memory after a system resume.
-    static void restore_all();
 };
